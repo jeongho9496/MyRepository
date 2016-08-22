1f6300bfbdb0cb3c970161372a9b2aca6be009c6
jeongho9496 <xjrxjrtkftkf@gmail.com>
2016-08-23 00:03:48 +0900

    게시판 과제 완료

--- JavaProgramming/src/ch05/exam03/BoardTest.java
+++ JavaProgramming/src/ch05/exam03/BoardTest.java
@@ -6,28 +6,25 @@ public class BoardTest {
 
 	public static void main(String[] args) {
 		// 0822
-		Scanner scanner = new Scanner(System.in);
-		String[][] boards = new String[100][];
-		;
+		Scanner scanner = new Scanner(System.in);//키보드 입력값 받는 메소드 호출
+		String[][] boards = new String[100][];	//배열 객체 정의
+									
+
 		while (true) {
 			System.out.println("-------------------------------------------------------------------------");
 			System.out.println("| 1.목록 | 2.글쓰기 | 3.상세보기 | 4.수정하기 | 5.삭제하기 | 6.종료 |");
 			System.out.println("-------------------------------------------------------------------------");
 			System.out.print("선택 : ");
 			String choice = scanner.nextLine(); // 키보드로 선택한 숫자를 읽어온다.
+
 			if (choice.equals("1")) {
 				System.out.println("************************************************************************");
-				System.out.println("게시물 번호\t\t글쓴이\t\t제목\t\t내용\t\t조회수");
+				System.out.println("게시물 번호\t\t글쓴이\t\t제목\t\t조회수");
 				System.out.println("************************************************************************");
 
-				for (String[] board : boards) { // for-each문
-					// for(int i = 0; students.length; i++){ for문 방식
-					// String[] student = students[i];
-					// }
-					if (board != null) { // null도 비교 가능 -> student에 배열이 있어야 실행
-											// 가능
-						System.out.println(board[0] + "\t\t" + board[1] + "\t\t" + board[2] + "\t\t" + board[3] + "\t\t"
-								+ board[4]);
+				for (String[] board : boards) {
+					if (board != null) {
+						System.out.println(board[0] + "\t\t" + board[1] + "\t\t" + board[2] + "\t\t" + board[4]);
 					}
 				}
 			} else if (choice.equals("2")) {
@@ -43,9 +40,6 @@ public class BoardTest {
 
 					if (boards[i] == null) {
 						String[] board = { String.valueOf(i), writer, title, content, "0" };
-						// String.valueOf(i) => 매개변수 안에 있는 숫자를 문자열로 바꿔준다.
-						// (<=> Integer.parseInt() => 매개변수 안에 있는 문자열이 숫자형식이면 숫자로
-						// 바꾼다.)
 						boards[i] = board;
 						break;
 					}
@@ -53,9 +47,44 @@ public class BoardTest {
 
 			} else if (choice.equals("3")) {
 
+				System.out.print("상세 보기할 번호 : ");
+				String detailNum = scanner.nextLine();
+				System.out.println("************************************************************************");
+				System.out.println("게시물 번호\t\t글쓴이\t\t제목\t\t내용\t\t조회수");
+				System.out.println("************************************************************************");
+				for (String[] board : boards) {
+					if (board != null) {
+						int views = Integer.parseInt(board[4]); //조회수 카운트 변수
+						if (detailNum.equals(board[0])) {
+							views++;							//읽은 만큼 증가
+							board[4] = String.valueOf(views);
+							System.out.println(board[0] + "\t\t" + board[1] + "\t\t" + board[2] + "\t\t" +board[3]+"\t\t"+ board[4]);
+							break;
+						}
+
+					}
+				}
+
 			} else if (choice.equals("4")) {
+				// 수정하기
+				System.out.print("수정할 게시글 번호 : ");
+				String replaceNum = scanner.nextLine();
+				System.out.print("수정 제목 : ");
+				String replaceTitle = scanner.nextLine();
+				for (String[] board : boards) {
+					if (board != null) {
+						if (replaceNum.equals(board[0])) {
+							board[2] = replaceTitle;
+							break;
+						}
+					}
+				}
 
 			} else if (choice.equals("5")) {
+				// 삭제하기
+				System.out.print("삭제할 번호 : ");
+				int bno = Integer.parseInt(scanner.nextLine());
+				boards[bno] = null;
 
 			} else if (choice.equals("6")) {
 				break;
