0aba3b82aef666941341a5fd959c1efbf9a50c12
jeongho9496 <xjrxjrtkftkf@gmail.com>
2016-08-24 01:06:24 +0900

    0824집

--- JavaProgramming/src/ch06/exam03/CarExample.java
+++ JavaProgramming/src/ch06/exam03/CarExample.java
@@ -30,6 +30,15 @@ public class CarExample {
 		
 		//객체 내부의 메소드를 호출하기
 		myCar.run();
+		System.out.println("------------------------------------");
+		System.out.println("제작회사 : " + myCar.company);
+		System.out.println("차 모델 : " + myCar.model);
+		System.out.println("차 색상 : " + myCar.color);
+		System.out.println("최고 속도 : " + myCar.maxSpeed);
+		System.out.println("현재속도 : " + myCar.speed);
+		
+		myCar.speed = 100;
+		System.out.println("수정된 속도 : " + myCar.speed);
 	}
 
 }
---
+++ JavaProgramming/src/ch06/exam03/FieldInitValue.java
@@ -0,0 +1,27 @@
+package ch06.exam03;
+
+public class FieldInitValue {	//라이브러리 클래스
+	//필드
+	byte byteField;
+	short shortField;
+	int intField;
+	long longField;
+	
+	boolean booleanField;
+	char charField;
+	
+	float floatField;
+	double doubleField;
+	
+	int[] arrField;
+	String referenceField;
+	
+	public FieldInitValue() {
+		System.out.println("오잉");
+		
+	}
+	
+	void run(){
+		System.out.println("실행됨");
+	}
+}
