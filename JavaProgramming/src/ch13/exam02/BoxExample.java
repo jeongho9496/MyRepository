442ed7293252167836e3f6de00c5b3a951455d05
jeongho9496 <xjrxjrtkftkf@gmail.com>
2016-09-13 15:21:49 +0900

    0913자습

--- JavaProgramming/src/ch13/exam02/BoxExample.java
+++ JavaProgramming/src/ch13/exam02/BoxExample.java
@@ -8,14 +8,17 @@ public class BoxExample {
 		Box<String> box1 = new Box<>();//다이아몬드 연산자<> 타입이 없지만 앞에 있는 타입을 따라 한다.
 		box1.setItem("홍길동");
 		String name = box1.getItem();
+		System.out.println(name);
 		
 		Box<Integer/*int의 클래스 타입*/> box2 = new Box<>();
 		box2.setItem(10);
-		int  value= (Integer)box2.getItem();
+		int value= box2.getItem();
+		System.out.println(value);
 		
 		Box<Date> box3 = new Box<>();
 		box3.setItem(new Date());
 		Date now = box3.getItem();
+		System.out.println(now);
 
 	}
 
--- JavaProgramming/src/ch13/exam05/BoxingMethodExample.java
+++ JavaProgramming/src/ch13/exam05/BoxingMethodExample.java
@@ -6,7 +6,7 @@ public class BoxingMethodExample {
 	//0908
 	public static void main(String[] args) {
 		Box<Integer> box1 = Util.boxing(new Integer(30));//객체 타입 받기
-		
+		//구체적인 타입을 지정하지 않아도 받는 쪽에서 매개값을 보고 유추한다.
 		Box<Date> box2 = Util.boxing(new Date());
 		
 		Box<String> box3 = Util.boxing("Tv");
