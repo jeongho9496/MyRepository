310613e4a68cb320226ef96d0e52e193e361320b
jeongho9496 <xjrxjrtkftkf@gmail.com>
2016-10-12 14:42:13 +0900

    SpringProgramming 삭제

--- SpringProgramming/src/main/java/com/mycompany/myapp/exam09/ServiceC.java
+++
@@ -1,6 +0,0 @@
-package com.mycompany.myapp.exam09;
-
-public interface ServiceC {
-	//1006
-	public void method1();
-}
--- SpringProgramming/src/main/java/com/mycompany/myapp/exam09/ServiceD.java
+++
@@ -1,6 +0,0 @@
-package com.mycompany.myapp.exam09;
-
-public interface ServiceD {
-	//1006
-	public void method1();
-}
