55c393cb9e21be09a12337ff5cf2603d6a0d7ae4
jeongho9496 <xjrxjrtkftkf@gmail.com>
2016-10-26 19:08:33 +0900

    1026수업 수정

--- SpringFinalProgramming/src/main/java/com/mycompany/myweb/service/MemberService.java
+++ SpringFinalProgramming/src/main/java/com/mycompany/myweb/service/MemberService.java
@@ -1,5 +1,105 @@
 package com.mycompany.myweb.service;
 
-public class MemberService {
+import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.mycompany.myweb.dao.MemberDao;
+import com.mycompany.myweb.dto.Member;
+
+@Component
+public class MemberService {
+	//1026
+	//3가지 경우가 발생할 경우 상수 지정을 하여 구분한다.
+	//2가지 경우가 발생항 경우 예외 처리가 하나 있울 시 메소드를 void로 작성한다.
+	//예외가 아닌 실패일 경우 상수 값으로 리턴 값을 받는다.
+	public static final int JOIN_SUCCESS=0;
+	public static final int JOIN_FAIL=0;
+	
+	public static final int LOGIN_SUCCESS=0;
+	public static final int LOGIN_FAIL_MID=1;
+	public static final int LOGIN_FAIL_MPASSWORD=2;	
+
+	public static final int LOGOUT_SUCCESS=0;
+	public static final int LOGOUT_FAIL=1;
+	
+	public static final int MODIFY_SUCCESS=0;
+	public static final int MODIFY_FAIL=1;
+	
+	public static final int WITHDRAW_SUCCESS=0;
+	public static final int WITHDRAW_FAIL=1;
+	
+	//join시 성공 아니면 예외 이므로 예외는 리턴 값이 없으므로 void를 사용한다.
+	
+	@Autowired
+	private MemberDao memberDao;
+	
+	public int join(Member member){
+		//예외처리를 하면 안된다.
+		memberDao.insert(member);
+		return JOIN_SUCCESS;
+	}
+	public int login(String mid, String mpassword, HttpSession session){
+		//회원 로그인
+		Member member = memberDao.selectByMid(mid);//멤버를 가져온다.
+		if (member == null) {
+			//디비에 멤버가 없다.
+			return LOGIN_FAIL_MID;
+		} 
+		
+		if(member.getMpassword().equals(mpassword) == false){
+			//디비에 있는 비밀번호와 다름.
+			return LOGIN_FAIL_MPASSWORD;
+		}
+		
+		session.setAttribute("login", mid);//성공시 session에 저장
+		
+		return LOGIN_SUCCESS;
+	}
+	public int logout(HttpSession session){
+		//회원 로그아웃
+		session.removeAttribute("login");
+		return LOGOUT_SUCCESS;
+	}
+	public String findMpassword(String mid, String memail){
+		//회원 패스워드 찾기
+		Member member = memberDao.selectByMid(mid);
+		if(member == null) return null;
+		if(member.getMemail().equals(memail) == false) return null;
+		return member.getMpassword();
+	}
+	public String findMid(String memail){
+		//회원 아이디 찾기
+		return memberDao.selectByMemail(memail);
+	}
+	public Member info(String mpassword, HttpSession session){
+		//회원 정보
+		String mid = (String) session.getAttribute("login");
+		Member member = memberDao.selectByMid(mid);
+		if (member.getMpassword().equals(mpassword) == false) return null;
+		
+		return member;
+		
+	}
+	public int modify(Member member){
+		//회원 수정
+		memberDao.update(member);
+		return MODIFY_SUCCESS;
+		
+	}
+	public int withdraw(String mpassword, HttpSession session){
+		//회원 탈퇴
+		String mid = (String) session.getAttribute("login");
+		Member member = memberDao.selectByMid(mid);
+		if (member.getMpassword().equals(mpassword) == false) {return WITHDRAW_FAIL;}
+		memberDao.delete(mid);
+		logout(session);
+		
+		return WITHDRAW_SUCCESS;
+	}
+	public boolean isMid(String mid){
+		//기존 회원 아이디 있는지 체크
+		return false;
+	}	
 }
