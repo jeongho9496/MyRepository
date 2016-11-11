be85f360ebaf03efaf98a1cb3ebf5463f8f63a9d
jeongho9496 <xjrxjrtkftkf@gmail.com>
2016-11-11 21:17:23 +0900

    1111androidlistview

--- AndroidProgramming/Exam07_ListView/app/src/main/java/com/example/administrator/myapplication/LightAdapter.java
+++ AndroidProgramming/Exam07_ListView/app/src/main/java/com/example/administrator/myapplication/LightAdapter.java
@@ -1,8 +1,12 @@
 package com.example.administrator.myapplication;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -11,10 +15,19 @@ import java.util.List;
  */
 
 public class LightAdapter extends BaseAdapter {
+
     private List<Light> list;
 
     public void setList(List<Light> list) {
         this.list = list;   //아이템을 외부에 받아 저장
+    }
+
+    private Context context;    //인터페이스. 모든 Activity가 context를 구현.
+    private LayoutInflater layoutInflater;
+
+    public LightAdapter(Context context) {
+        this.context = context;   //주입
+        layoutInflater = LayoutInflater.from(context);
     }
 
     @Override
@@ -28,12 +41,29 @@ public class LightAdapter extends BaseAdapter {
     }
 
     @Override
-    public long getItemId(int position) {
-        return 0;
+    public long getItemId(int position) {   //데이터에 식별 번호가 있으면 식별번호를 리턴하고 없으면 position 값을 리턴한다.
+        return position;    // 리스트 상의 인덱스 - position, 객체 안에 있는 데이터중에서 값을 식별 할수 있는것. - itemId
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+    public View getView(int position, View convertView, ViewGroup parent) {//해당위치의 데이터를 보여주는 itemview 객체를 만들어 주는 역할.
+        //position -> 아이템 리스트 들의 번호, convertView -> item.xml 객체, 재사용되는 뷰가 넘어감. parent -> ListView
+        if (convertView == null) {
+            //inflation
+            convertView = layoutInflater.inflate(R.layout.light_item, null);
+        }
+
+        //convertView 가 null이 아닐 경우 data setting 한다.
+        Light light = list.get(position);
+        ImageView lightImage = (ImageView) convertView.findViewById(R.id.lightImage);
+        lightImage.setImageResource(light.getImage());
+
+        TextView lightTitle = (TextView)convertView.findViewById(R.id.lightTitle);
+        lightTitle.setText(light.getTitle());
+
+        TextView lightContent = (TextView)convertView.findViewById(R.id.lightContent);
+        lightContent.setText(light.getContent());
+
+        return convertView;
     }
 }
--- AndroidProgramming/Exam07_ListView/app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ AndroidProgramming/Exam07_ListView/app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -3,25 +3,24 @@ package com.example.administrator.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.widget.Adapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
-    private LinearLayout itemContainer;
-    private LayoutInflater layoutInflater;//xml파일을 객체로 만듬
+    private ListView lightList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        itemContainer = (LinearLayout)findViewById(R.id.itemContainer);//itemContainer 추가
-        layoutInflater = getLayoutInflater();
+       lightList = (ListView)findViewById(R.id.lightList); //초기화 작업
 
         fillItems();
 
@@ -34,24 +33,16 @@ public class MainActivity extends AppCompatActivity {
         list.add(new Light(R.drawable.light3, "인테리어 조명1", "화장실등으로 사용하면 좋습니다. 검은색 테두리와 백열등의 조화가 이쁩니다."));
         list.add(new Light(R.drawable.light4, "인테리어 조명1", "현관등으로 사용하면 좋습니다. 검은색 테두리와 백열등의 조화가 이쁩니다."));
         list.add(new Light(R.drawable.light5, "인테리어 조명1", "안방등으로 사용하면 좋습니다. 검은색 테두리와 백열등의 조화가 이쁩니다."));
+        list.add(new Light(R.drawable.light1, "인테리어 조명1", "거실등으로 사용하면 좋습니다. 검은색 테두리와 백열등의 조화가 이쁩니다."));
+        list.add(new Light(R.drawable.light2, "인테리어 조명1", "자녀방등으로 사용하면 좋습니다. 검은색 테두리와 백열등의 조화가 이쁩니다."));
+        list.add(new Light(R.drawable.light3, "인테리어 조명1", "화장실등으로 사용하면 좋습니다. 검은색 테두리와 백열등의 조화가 이쁩니다."));
+        list.add(new Light(R.drawable.light4, "인테리어 조명1", "현관등으로 사용하면 좋습니다. 검은색 테두리와 백열등의 조화가 이쁩니다."));
+        list.add(new Light(R.drawable.light5, "인테리어 조명1", "안방등으로 사용하면 좋습니다. 검은색 테두리와 백열등의 조화가 이쁩니다."));
 
 
-        for (Light light : list) {
-            //inplation
-            LinearLayout itemLayout = (LinearLayout) layoutInflater.inflate(R.layout.light_item, null);//xml의 제일 상위 Layout가 리턴됨.
+        LightAdapter lightAdapter = new LightAdapter(this); //this - MainActivity 가 들어감.
+        lightAdapter.setList(list);
 
-            //Data Setting
-            ImageView lightImage = (ImageView) itemLayout.findViewById(R.id.lightImage);
-            lightImage.setImageResource(light.getImage());
-
-            TextView lightTitle = (TextView) itemLayout.findViewById(R.id.lightTitle);
-            lightTitle.setText(light.getTitle());
-
-            TextView lightContent = (TextView) itemLayout.findViewById(R.id.lightContent);
-            lightContent.setText(light.getContent());
-
-            //itemContainer의 내부 객체로 추가
-            itemContainer.addView(itemLayout);
-        }
+        lightList.setAdapter(lightAdapter);
     }
 }
