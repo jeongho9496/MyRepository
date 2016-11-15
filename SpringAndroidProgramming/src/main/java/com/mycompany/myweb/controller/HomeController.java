a55267966e616a6795b0c5b09404f7e0a10a8b34
jeongho9496 <xjrxjrtkftkf@gmail.com>
2016-11-15 12:21:54 +0900

    1115안드로이드 수정

--- SpringAndroidProgramming/src/main/java/com/mycompany/myweb/controller/HomeController.java
+++ SpringAndroidProgramming/src/main/java/com/mycompany/myweb/controller/HomeController.java
@@ -1,10 +1,16 @@
 package com.mycompany.myweb.controller;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mycompany.myweb.dto.Light;
 
 @Controller
 public class HomeController {
@@ -17,6 +23,28 @@ public class HomeController {
 		
 		logger.info("index() 실행");	
 		return "index";
+	}
+	
+	@RequestMapping("/lightList")
+	public String lightList( Model model ) {
+		
+		logger.info("lightList() 실행");	
+		
+		List<Light> list = new ArrayList<>();
+        list.add(new Light("light1.png","light1_large.png","인테리어 조명1", "거실등으로 사용하면 좋습니다. 검은색 테두리와 백열등의 조화가 이쁩니다."));
+        list.add(new Light("light2.png","light2_large.png","인테리어 조명1", "자녀방등으로 사용하면 좋습니다. 검은색 테두리와 백열등의 조화가 이쁩니다."));
+        list.add(new Light("light3.png","light3_large.png","인테리어 조명1", "화장실등으로 사용하면 좋습니다. 검은색 테두리와 백열등의 조화가 이쁩니다."));
+        list.add(new Light("light4.png","light4_large.png","인테리어 조명1", "현관등으로 사용하면 좋습니다. 검은색 테두리와 백열등의 조화가 이쁩니다."));
+        list.add(new Light("light5.png","light5_large.png","인테리어 조명1", "안방등으로 사용하면 좋습니다. 검은색 테두리와 백열등의 조화가 이쁩니다."));
+        list.add(new Light("light1.png","light1_large.png","인테리어 조명1", "거실등으로 사용하면 좋습니다. 검은색 테두리와 백열등의 조화가 이쁩니다."));
+        list.add(new Light("light2.png","light2_large.png","인테리어 조명1", "자녀방등으로 사용하면 좋습니다. 검은색 테두리와 백열등의 조화가 이쁩니다."));
+        list.add(new Light("light3.png","light3_large.png","인테리어 조명1", "화장실등으로 사용하면 좋습니다. 검은색 테두리와 백열등의 조화가 이쁩니다."));
+        list.add(new Light("light4.png","light4_large.png","인테리어 조명1", "현관등으로 사용하면 좋습니다. 검은색 테두리와 백열등의 조화가 이쁩니다."));
+        list.add(new Light("light5.png","light5_large.png","인테리어 조명1", "안방등으로 사용하면 좋습니다. 검은색 테두리와 백열등의 조화가 이쁩니다."));
+		
+        model.addAttribute("list", list);//객체를 jsp로 넘김
+		
+		return "lightList";
 	}
 	
 }
---
+++ SpringAndroidProgramming/src/main/webapp/WEB-INF/views/lightList.jsp
@@ -0,0 +1,15 @@
+<%@ page contentType="application/json;charset=UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+[
+<c:forEach var="light" items="${list}"<%-- list로 된 객체를 찾음 --%> varStatus="status">
+<%-- varStatus="status" 루프정보를 담는 객체를 저장할 변수명을 값으로 갖는다. --%>
+	{
+		"image":"${light.image}",
+		"imageLarge":"${light.imageLarge}",
+		"title":"${light.title}",
+		"content":"${light.content}"
+	}
+	<c:if test="${!status.last}">,</c:if><%-- 마지막 행 전까지 ','를 넣어라 --%>
+</c:forEach>
+]
