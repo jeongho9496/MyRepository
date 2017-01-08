fea9e162a1c4590f0aefa474ebcadea4a77f1865
jeongho9496 <xjrxjrtkftkf@gmail.com>
2017-01-08 15:00:25 +0900

    dto 추가

---
+++ EmployeeProgramming/src/main/java/com/mycompany/empweb/dto/Employee.java
@@ -0,0 +1,40 @@
+package com.mycompany.empweb.dto;
+
+public class Employee {
+	private int no;
+	private String name;
+	private String phone;
+	private String position;
+	private String email;
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getPosition() {
+		return position;
+	}
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
